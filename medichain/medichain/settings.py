ef948990c6c9bb45c40fd5b5737d312bc9516314
YeshBhattz <86815567+YeshBhattz@users.noreply.github.com>
2026-02-19 13:28:32 +0530

    Connected app with django project successfully

--- medichain/medichain/settings.py
+++ medichain/medichain/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "userauth",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "medichain.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
