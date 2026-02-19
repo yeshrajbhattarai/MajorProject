6ce9f4a59654f171f8edaa4390435e8368af1c69
YeshBhattz <86815567+YeshBhattz@users.noreply.github.com>
2026-02-19 15:17:33 +0530

    Added Connection to the Database

--- medichain/medichain/settings.py
+++ medichain/medichain/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = "medichain.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medichain',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
