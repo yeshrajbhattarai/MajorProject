3b4f93bd813acccc475005ce9ca6d070ec6eb85d
YeshBhattz <86815567+YeshBhattz@users.noreply.github.com>
2026-05-06 16:41:32 +0530

    changed age and gender to required false

--- medichain/hospitals/serializers.py
+++ medichain/hospitals/serializers.py
@@ -264,7 +264,7 @@ class MedicalRecordMetaSerializer(serializers.ModelSerializer):
 
 
 class CreateMedicalRecordSerializer(serializers.Serializer):
-    age = serializers.IntegerField()
-    gender = serializers.CharField()
+    age = serializers.IntegerField(required=False)
+    gender = serializers.CharField(required=False)
     technician_change_reason = serializers.CharField(required=False, allow_blank=True)
     custom_field_values = serializers.JSONField(required=False)
